d5d2b9a32d9ed150228eaa9b97687f0bfe306836
farah saleem <saleemfarah12@gmail.com>
2022-05-28 18:14:59 +0300

    add singleton pattern

---
+++ App/Clients/SingletonClients.cs
@@ -0,0 +1,24 @@
+﻿using SingletonPattern.Singleton;
+
+namespace App.Clients;
+
+public class SingletonClients
+{
+    public static void Execute()
+    {
+        Parallel.Invoke(
+            () => Run(),
+            () => Run(),
+            () => Run(),
+            () => Run(),
+            () => Run());
+    }
+
+    private static void Run()
+    {
+        var boiler = ChocolateBoiler.Instance;
+        boiler.Fill();
+        boiler.Boil();
+        boiler.Drain();
+    }
+}
---
+++ SingletonPattern/Singleton/ChocolateBoiler.cs
@@ -0,0 +1,71 @@
+﻿namespace SingletonPattern.Singleton;
+
+public class ChocolateBoiler
+{
+    private static ChocolateBoiler? _instance;
+    private static readonly object _instanceLock = new();
+
+    private ChocolateBoiler()
+    {
+        Id = new Random().Next(1, 1000);
+    }
+
+    public int Id { get; private init; }
+
+    public bool Empty { get; private set; }
+
+    public bool Boiled { get; private set; }
+
+    public static ChocolateBoiler Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                lock (_instanceLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ChocolateBoiler();
+                    }
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    public void Boil()
+    {
+        Console.WriteLine("Boiling");
+        Console.WriteLine($"Boiler ID: {Id}");
+
+        if (!Empty && !Boiled)
+        {
+            Boiled = true;
+        }
+    }
+
+    public void Drain()
+    {
+        Console.WriteLine("Draining");
+        Console.WriteLine($"Boiler ID: {Id}");
+
+        if (!Empty && Boiled)
+        {
+            Empty = true;
+            Boiled = false;
+        }
+    }
+
+    public void Fill()
+    {
+        Console.WriteLine("Filling");
+        Console.WriteLine($"Boiler ID: {Id}");
+
+        if (Empty)
+        {
+            Empty = false;
+        }
+    }
+}
